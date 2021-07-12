2e5c9ffa58b1ddb4916816221d44664cbd23e54f
apronotti <apronotti@sismatech.com.ar>
2021-07-12 11:13:21 -0300

    I changed the upload way for contract ABI to imported, I defined the constants 'step + parallel' previous the method calls.

--- whitelist-helper-scripts/genWhitlisted.ts
+++ whitelist-helper-scripts/genWhitlisted.ts
@@ -2,9 +2,8 @@ import procEvents from "./procEvents";
 import procLastAuthenticated from "./procLastAuthenticated";
 import saveToFile from "./utils";
 import { ethers } from "ethers";
-import * as fs from 'fs';
+import { identity as identityContractABI } from "./identityContractABI.json";
 
-const identityContractABI: string = JSON.parse(fs.readFileSync("identityContractABI.json").toString());
 const url: string = "https://rpc.fuse.io/";
 const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(url);
 
@@ -12,15 +11,20 @@ const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcP
 const IDENTITY_CONTRACT_ADDRESS = "0xFa8d865A962ca8456dF331D78806152d3aC5B84F";
 const contract: ethers.Contract = new ethers.Contract(IDENTITY_CONTRACT_ADDRESS, identityContractABI, provider);
 
-const startBlock: number =  6246324;
 const whitelisted: object = {};
 const whitelistedWLastAuthenticated: object = {};
 
 async function main() {
+    
+    // It feeds the whitelisted, adding addresses from WhitelistedAdded events
+    const startBlock: number =  6246324;
+    const step: number = 20000;
+    const parallel: number = 5;
+
     console.log('=======================================================================================');
     console.log('Generating Whitelist... from block number: '+startBlock);
     console.log('=======================================================================================');
-    // It Feeds the whitelistd dict
+
     await procEvents(contract,
         contract.filters.WhitelistedAdded(), // filter value
         startBlock,
@@ -32,7 +36,13 @@ async function main() {
                     whitelisted[event.args.account] = event.blockNumber;
                 }
             }); 
-    });
+        },
+        step,
+        parallel);
+        
+    // It purge the whitelisted, removing addresses from WhitelistedRemoved event
+    const lAuthParallel: number = 1000;
+    
     console.log('=======================================================================================');
     console.log('Purging Whitelist, deleting WhitelistedRemoved from block number: '+startBlock);
     console.log('=======================================================================================');
@@ -50,7 +60,9 @@ async function main() {
                 }
             }
           });
-        });
+        },
+        step,
+        parallel);
 
     // It save the whitelist into json file
     await saveToFile('whitelistedWBlock.json', whitelisted);
@@ -59,7 +71,10 @@ async function main() {
     console.log('=======================================================================================');
     console.log('Generating Whitelist with lastAuthenticated ... ');
     console.log('=======================================================================================');    
-    await procLastAuthenticated(contract, whitelisted, whitelistedWLastAuthenticated);
+    await procLastAuthenticated(contract, 
+                                whitelisted, 
+                                whitelistedWLastAuthenticated,
+                                lAuthParallel);
 
     await saveToFile('whitelistedWlastAuthenticated-ethersjs.json',whitelistedWLastAuthenticated);
     
