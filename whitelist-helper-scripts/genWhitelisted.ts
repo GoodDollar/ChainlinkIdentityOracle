be335251c84146e161df378fd9185d5e62e97dc0
Alejandro Pronotti <45662836+apronotti@users.noreply.github.com>
2021-08-11 12:28:50 -0300

    Helper sdk enhancement cl ea (Chore) #1 (#3)
    * purging old files.
    
    * Update README.md
    
    * output json file format changed to be compatible with sdk.
    
    * initial import of Identity oracle sdk
    
    * use dotenv to save keys
    
    * to install dotenv
    
    * The whitelist helper were enhanced to integrate with the Oracle CL adapter.
    
    * The sdk was enhanced to integrate with the Oracle CL adapter.
    
    * show error and exit when authentication or IPFSfile upload fails
    
    Co-authored-by: Mateo Hepp <mateo@digitalbridge.link>

--- whitelist-helper-scripts/genWhitelisted.ts
+++ whitelist-helper-scripts/genWhitelisted.ts
@@ -1,86 +1,85 @@
-import procEvents from "./procEvents";
-import procLastAuthenticated from "./procLastAuthenticated";
-import saveToFile from "./utils";
-import { ethers } from "ethers";
-import { identity as identityContractABI } from "./identityContractABI.json";
+import procEvents from './procEvents'
+import procLastAuthenticated from './procLastAuthenticated'
+import saveToFile from './utils'
+import { ethers } from 'ethers'
+import { identity as identityContractABI } from './identityContractABI.json'
 
-const url: string = "https://rpc.fuse.io/";
-const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(url);
+const url = 'https://rpc.fuse.io/'
+const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(url)
 
 // Identity contract address
-const IDENTITY_CONTRACT_ADDRESS = "0xFa8d865A962ca8456dF331D78806152d3aC5B84F";
-const contract: ethers.Contract = new ethers.Contract(IDENTITY_CONTRACT_ADDRESS, identityContractABI, provider);
+const IDENTITY_CONTRACT_ADDRESS = '0xFa8d865A962ca8456dF331D78806152d3aC5B84F'
+const contract: ethers.Contract = new ethers.Contract(
+  IDENTITY_CONTRACT_ADDRESS,
+  identityContractABI,
+  provider,
+)
 
-const whitelisted: object = {};
-const whitelistedWLastAuthenticated: object = {};
-
-async function main() {
-    
-    // It feeds the whitelisted, adding addresses from WhitelistedAdded events
-    const startBlock: number =  6246324;
-    const step: number = 20000;
-    const parallel: number = 5;
-
-    console.log('=======================================================================================');
-    console.log('Generating Whitelist... from block number: '+startBlock);
-    console.log('=======================================================================================');
-
-    await procEvents(contract,
-        contract.filters.WhitelistedAdded(), // filter value
-        startBlock,
-        (events: any[]) => {
-            events.forEach( event => {
-                if (!whitelisted.hasOwnProperty[event.args.account] ||
-                       (whitelisted.hasOwnProperty[event.args.account] && 
-                        whitelisted[event.args.account] < event.blockNumber)) {
-                    whitelisted[event.args.account] = event.blockNumber;
-                }
-            }); 
-        },
-        step,
-        parallel);
-        
-    
-    console.log('=======================================================================================');
-    console.log('Purging Whitelist, deleting WhitelistedRemoved from block number: '+startBlock);
-    console.log('=======================================================================================');
-    
-    await procEvents(contract,
-        contract.filters.WhitelistedRemoved(), // filter value
-        startBlock,
-        (events: any[]) => {
-          events.forEach( event => {
-            var address = event.args.account;
-            if (whitelisted.hasOwnProperty(address)) {
-                if (whitelisted[address] < event.blockNumber) {
-                    delete whitelisted[address];
-                    console.log(address+' deleted.');
-                }
-            }
-          });
-        },
-        step,
-        parallel);
-
-    // It save the whitelist into json file
-    await saveToFile('whitelistedWBlock.json', whitelisted);
-    
-    
-    // It purge the whitelisted, removing addresses from WhitelistedRemoved event
-    const lAuthParallel: number = 1000;
-    
-    console.log(" Whitelisted size: "+Object.keys(whitelisted).length);
-    console.log('=======================================================================================');
-    console.log('Generating Whitelist with lastAuthenticated ... ');
-    console.log('=======================================================================================');    
-    await procLastAuthenticated(contract, 
-                                whitelisted, 
-                                whitelistedWLastAuthenticated,
-                                lAuthParallel);
-
-    await saveToFile('whitelistedWlastAuthenticated.json',whitelistedWLastAuthenticated);
-    
+type Whitelisted = {
+  [key: string]: number
 }
 
-main();
+const whitelisted: Whitelisted = {}
+const whitelistedWLastAuthenticated = {}
 
+async function genWhitelist() {
+  // It feeds the whitelisted, adding addresses from WhitelistedAdded events
+  const startBlock = 6246324
+  const step = 20000
+  const parallel = 5
+
+  await procEvents(
+    contract,
+    contract.filters.WhitelistedAdded(), // filter value
+    startBlock,
+    (events) => {
+      events.forEach((event) => {
+        if (
+          !whitelisted.hasOwnProperty(event.args ? event.args.account : '') ||
+          (whitelisted.hasOwnProperty(event.args ? event.args.account : '') &&
+            whitelisted[event.args ? event.args.account : ''] < event.blockNumber)
+        ) {
+          whitelisted[event.args ? event.args.account : ''] = event.blockNumber
+        }
+      })
+    },
+    step,
+    parallel,
+  )
+
+  await procEvents(
+    contract,
+    contract.filters.WhitelistedRemoved(), // filter value
+    startBlock,
+    (events: any[]) => {
+      events.forEach((event) => {
+        const address: string = event.args.account
+        if (whitelisted.hasOwnProperty(address)) {
+          if (whitelisted[address] < event.blockNumber) {
+            delete whitelisted[address]
+            //console.log(address+' deleted.');
+          }
+        }
+      })
+    },
+    step,
+    parallel,
+  )
+
+  // It save the whitelist into json file
+  await saveToFile({ _filename: 'whitelistedWBlock.json', _content: whitelisted })
+
+  // It purge the whitelisted, removing addresses from WhitelistedRemoved event
+  const lAuthParallel = 1000
+
+  await procLastAuthenticated(contract, whitelisted, whitelistedWLastAuthenticated, lAuthParallel)
+
+  await saveToFile({
+    _filename: 'whitelistedWlastAuthenticated.json',
+    _content: whitelistedWLastAuthenticated,
+  })
+}
+
+genWhitelist()  // Comment this line for use in the CL oracle adapter
+
+//export { genWhitelist } // Uncomment this line to use in the CL oracle adapter
