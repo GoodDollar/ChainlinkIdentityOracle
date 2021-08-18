41eb44e8f99400de2316cab41b955427e1d3fe90
apronotti <apronotti@sismatech.com.ar>
2021-08-18 15:35:51 -0300

    adding more verbosity to integration test.

--- test/integration/IdentityOracle_int_test.js
+++ test/integration/IdentityOracle_int_test.js
@@ -4,7 +4,7 @@ const skipIf = require('mocha-skip-if')
 const { developmentChains } = require('../../helper-hardhat-config')
 
 skip.if(developmentChains.includes(network.name)).
-  describe('IdentityOracle Integration Tests', async function () {
+  describe('IdentityOracle Integration Tests \n', async function () {
 
     let identityOracle
 
@@ -14,19 +14,24 @@ skip.if(developmentChains.includes(network.name)).
     })
 
     it('Should successfully make an startIPFSandStateHashProcess request and get a result', async () => {
+      const IdentityOracle = await deployments.get('IdentityOracle')
+      console.log("Calling IdentityOracle contract ", IdentityOracle.address, " on network ", network.name)
       const transaction = await identityOracle.startIPFSandStateHashProcess()
       const tx_receipt = await transaction.wait()
       const requestId = tx_receipt.events[0].topics[1]
-
-      //wait 45 secs for oracle to callback
-      await new Promise(resolve => setTimeout(resolve, 45000))
-
+      console.log('Contract ', IdentityOracle.address, ' external data request successfully called. Transaction Hash: ', tx_receipt.transactionHash + '\n')
+      
+      //wait 60 secs for oracle to callback
+      console.log('Wait 60 seconds to read the result ... \n')
+      await new Promise(resolve => setTimeout(resolve, 60000))
+      console.log("Getting the result: \n")
+      
       //Now check the result
       const result = await identityOracle.stateHash()
       console.log("IdentityOracle stateHash value: ", result) 
       const resultIPFSCID = await identityOracle.stateDataIPFS()
       console.log("IdentityOracle IPFS CID value: ", resultIPFSCID) 
-            
+
       expect(result).to.not.be.equal('')
     })
   })
